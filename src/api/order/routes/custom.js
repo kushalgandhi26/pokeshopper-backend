01d5f4ebef2bc88b97916f60656d7737c3011272
Kushal Gandhi <kushalgandhi2601@gmail.com>
2022-06-17 12:47:36 +0530

    initial commit

---
+++ src/api/order/routes/custom.js
@@ -0,0 +1,12 @@
+// path: ./src/api/restaurant/routes/custom-restaurant.js
+
+module.exports = {
+    routes: [
+      {
+        method: 'POST',
+        path: '/orders/pretransaction',
+        handler: 'custom.exampleAction',
+      },
+    ],
+  };
+  
