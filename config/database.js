ba3103a5ac0d191ea29563f7b501a6a930cd9c89
Kushal Gandhi <kushalgandhi2601@gmail.com>
2022-06-17 13:01:43 +0530

    hosted to heroku

--- config/database.js
+++ config/database.js
@@ -1,13 +1,33 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST'),
+//       port: env.int('DATABASE_PORT'),
+//       database: env('DATABASE_NAME'),
+//       user: env('DATABASE_USERNAME'),
+//       password: env('DATABASE_PASSWORD'),
+//       schema: env('DATABASE_SCHEMA'), // Not required
+//     }
+//   },
+// });
+
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      schema: env('DATABASE_SCHEMA'), // Not required
-    }
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    debug: false,
   },
 });
